b1f8e2f5f6e238daa4923dd3535ba4f2fadec53e
kirtiiii8 <157558695+kirtiiii8@users.noreply.github.com>
2026-06-24 19:19:33 +0530

    just an update

--- index.js
+++ index.js
@@ -146,6 +146,8 @@ app.post("/bfhl", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server Running On Port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server Running On Port ${PORT}`);
 });
